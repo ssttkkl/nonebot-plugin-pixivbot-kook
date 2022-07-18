0ecd1df286bc1b848566fdb31f4393273ae18ede
ssttkkl <huang.wen.long@hotmail.com>
2022-07-18 21:16:39 +0800

    改名部工作了

--- src/nonebot_plugin_pixivbot_kook/__init__.py
+++ src/nonebot_plugin_pixivbot_kook/__init__.py
@@ -1,6 +1,13 @@
-# ================= provide beans =================
-from nonebot_plugin_pixivbot import context
+"""
+nonebot-plugin-pixivbot-kook
 
-from .providers import *
+@Author         : ssttkkl
+@License        : MIT
+@GitHub         : https://github.com/ssttkkl/nonebot-plugin-pixivbot-kook
+"""
 
-provide(context)
+# ======= register Postman and PostDestination =======
+from .postman import Postman, PostDestinationFactory
+
+# =============== register protocol_dep ===============
+from .protocol_dep.user_authenticator import UserAuthenticator
--- src/nonebot_plugin_pixivbot_kook/protocol_dep/user_authenticator.py
+++ src/nonebot_plugin_pixivbot_kook/protocol_dep/user_authenticator.py
@@ -1,8 +1,10 @@
-from nonebot_plugin_pixivbot.protocol_dep import UserAuthenticator as BaseUserAuthenticator
+from nonebot_plugin_pixivbot import context
+from nonebot_plugin_pixivbot.protocol_dep.user_authenticator import UserAuthenticator as BaseUserAuthenticator
 
 from nonebot_plugin_pixivbot_kook.postman import PostDestination
 
 
+@context.bind_singleton_to(BaseUserAuthenticator)
 class UserAuthenticator(BaseUserAuthenticator):
     async def group_admin(self, post_dest: PostDestination) -> bool:
         # 搞不懂怎么实现
